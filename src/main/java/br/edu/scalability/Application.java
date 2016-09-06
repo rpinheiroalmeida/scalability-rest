046e36ccead1a1c20e12f7775b8215025ee2d4b2
Marco Reis <ma@marcoreis.net>
2016-09-06 01:37:08 -0300

    update

--- src/main/java/br/edu/scalability/Application.java
+++ src/main/java/br/edu/scalability/Application.java
@@ -2,6 +2,7 @@ package br.edu.scalability;
 
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
@@ -11,14 +12,14 @@ import com.datastax.driver.core.Session;
 /**
  * Created by rodrigopinheiro on 8/27/16.
  */
-
+@EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
 
 	private static Logger logger = Logger.getLogger(Application.class);
 	private Session session;
 	private Cluster cluster;
-	private String contactPoints = "kenobi";
+	private String contactPoints = System.getProperty("servidor.cassandra");
 	private int port = 9042;
 
 	public static void main(String[] args) {
