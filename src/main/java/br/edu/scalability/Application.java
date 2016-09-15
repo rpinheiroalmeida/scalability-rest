cea377beb8351f312b1603e58594dfbdb9a972bf
Marco Reis <ma@marcoreis.net>
2016-09-15 16:08:05 -0300

    update

--- src/main/java/br/edu/scalability/Application.java
+++ src/main/java/br/edu/scalability/Application.java
@@ -2,8 +2,10 @@ package br.edu.scalability;
 
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Session;
@@ -13,6 +15,8 @@ import com.datastax.driver.core.Session;
  */
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class Application {
 
 	private static Logger logger = Logger.getLogger(Application.class);
@@ -39,6 +43,7 @@ public class Application {
 	@Bean(destroyMethod = "close")
 	public Cluster cluster() throws Exception {
 		if (cluster == null) {
+			logger.info("Servidor Cassandra: " + contactPoints);
 			cluster = Cluster.builder().addContactPoints(contactPoints).withPort(port).withCredentials(user, pwd)
 					.build();
 			logger.info("Build cluster");
@@ -46,4 +51,16 @@ public class Application {
 		return cluster;
 	}
 
+	// protected SpringApplicationBuilder configure(SpringApplicationBuilder
+	// application) {
+	// Application obj = new Application();
+	// @SuppressWarnings("resource")
+	// ConfigurableApplicationContext applicationContext = new
+	// ClassPathXmlApplicationContext(
+	// "META-INF/spring/applicationContext.xml");
+	// applicationContext.registerShutdownHook();
+	// applicationContext.getBeanFactory().autowireBeanProperties(obj,
+	// AutowireCapableBeanFactory.AUTOWIRE_NO, false);
+	// return application.sources(Application.class);
+	// }
 }
